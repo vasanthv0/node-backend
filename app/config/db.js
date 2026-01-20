e358fe97e53ff57e4968e3fdc580d703746aa4bb
Vasanthakumar <vasanthakumarv@255@gmail.com>
2026-01-20 12:56:18 +0530

    Use DATABASE_URL for Render production

--- app/config/db.js
+++ app/config/db.js
@@ -2,11 +2,15 @@ const { Pool } = require("pg");
 require("dotenv").config();
 
 const pool = new Pool({
-  user: process.env.DB_USER,
-  host: process.env.DB_HOST,
-  database: process.env.DB_NAME,
-  password: process.env.DB_PASS,
-  port: process.env.DB_PORT,
+  // user: process.env.DB_USER,
+  // host: process.env.DB_HOST,
+  // database: process.env.DB_NAME,
+  // password: process.env.DB_PASS,
+  // port: process.env.DB_PORT,
+  connectionString: process.env.DATABASE_URL,
+    ssl: {
+    rejectUnauthorized: false
+  }
 });
 
 pool.connect()
